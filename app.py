2595f000b03f35be9d624d985cde146893b436f0
Shirish Koirala <shirishkoirala@gmail.com>
2025-02-26 15:25:19 +1000

    Chatbot is working.

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
 import streamlit as st
+from langchain_ollama import ChatOllama
 
+def generate_response(input):
+    model = ChatOllama(
+        model="llama3.2:latest"
+    )
+    response = model.invoke(input)
+    return response.content
 
-st.title("Hello World")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.chat_message("user").markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.spinner("Thinking..."):
+        response = generate_response(st.session_state.messages)
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        st.session_state.messages.append({"role": "assistant", "content": response})
